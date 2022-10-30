826e0c22700f1ee954088a027ca1bdb7fff0c9a0
junpei-8 <miurajunpeis2@gmail.com>
2022-10-30 12:45:20 +0900

    update: added wasm

---
+++ src/wasm/pulldown-cmark/src/lib.rs
@@ -0,0 +1,19 @@
+use pulldown_cmark::{html, Options, Parser};
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn convert_markdown_to_html(markdown: &str) -> String {
+    let markdown_input = markdown;
+
+    let mut options = Options::empty();
+
+    options.insert(Options::ENABLE_STRIKETHROUGH);
+
+    let parser = Parser::new_ext(markdown_input, options);
+
+    let mut result = String::new();
+
+    html::push_html(&mut result, parser);
+
+    result
+}
